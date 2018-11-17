726c0e9353b4e178fb9303b44fe590057b25c7ec
Michael Brown <michael@brown37.net>
2018-11-17 15:07:30 -0700

    Compress XML on creation

--- preprocessing/base.lua
+++ preprocessing/base.lua
@@ -40,6 +40,9 @@ end
 local counter = 0
 base.deparse = function (xml, maxChap, maxVerse)
   if type(xml) ~= "table" then
+    if type(xml) == "string" and xml:match("%S") == nil then
+      xml = ""
+    end
     return xml
   end
   if maxChap and xml.tag == "chapter" and tonumber(xml.attr.number) > maxChap then return ""
